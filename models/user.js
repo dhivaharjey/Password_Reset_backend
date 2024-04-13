3bd553deb8461dc0bdd575b1b1eba1e081e681f8
dhivaharjey <dhivahar19399@gmail.com>
2024-04-13 23:42:53 +0530

    bcrypt

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import crypto from "crypto";
+import bcrypt from "bcryptjs-react";
+// import crypto from "crypto";
 const userSchema = new mongoose.Schema({
   // name: {
   //   type: String,
